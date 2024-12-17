c4abb41cbe30b54b14dcd520659d11cb4b7afc80
Joshua Noyes <Koholos@yahoo.com>
2024-12-16 20:18:38 -0500

    final practice version

--- library.js
+++ library.js
@@ -1,12 +1,10 @@
 const debug = true;
-
-
 const myLibrary = [];
+const shelves = document.getElementById("grid")
 
 function db(input) {
     if (debug) console.log(input);
 }
-
 
 //creates a new book object.
 class Book{
@@ -18,9 +16,14 @@ class Book{
     }
 }
 
+function clearLibrary(){
+    shelves.innerHTML = "";
+}
+
 //logs the new Book and then clears the prompt screen.
 function createBook(event){
     event.preventDefault();
+    clearLibrary();
 
     const title = event.target.title.value;
     const author = event.target.author.value;
@@ -28,11 +31,11 @@ function createBook(event){
     const readState = event.target.readStatus.checked;
 
     const newBook = new Book(title,author,pages,readState)
-    db(newBook)
 
-    myLibrary.push(newBook)
+    length = myLibrary.push(newBook)
     event.target.reset();
-    db(myLibrary)
+
+    myLibrary.forEach(addBookToLibrary);
 }
 
 function openLibrary(){
@@ -43,11 +46,45 @@ function openLibrary(){
     myLibrary.push(book1,book2,book3)
 }
 
+function addBookToLibrary(input){
+
+
+    const bookCover = document.createElement("div");
+    bookCover.setAttribute("id",input.title);
+    bookCover.classList.add("book")
+
+    const title = document.createElement("h2");
+    title.classList.add("title");
+    title.textContent=input.title;
+
+    const author = document.createElement("h3");
+    author.classList.add("author");
+    author.textContent = input.author;
+
+    const pages = document.createElement("p");
+    pages.classList.add("pageNum");
+    pages.textContent = input.pages + " pages";
+
+    const readStatus = document.createElement("p");
+    readStatus.classList.add("readStatus");
+    if(input.readState === true){
+        readStatus.textContent = "Read";
+    }
+    else{
+        readStatus.textContent = "Unread";
+    }
+
+    bookCover.append(title, author,pages,readStatus);
+    shelves.appendChild(bookCover)
+
+    db(bookCover)
+}
 
 
 //initialize the script
 openLibrary();
-db(myLibrary);
+shelves.innerHTML = "";
+myLibrary.forEach(addBookToLibrary)
 
 const bookForm = document.getElementById("new-book");
 bookForm.addEventListener("submit",createBook);
